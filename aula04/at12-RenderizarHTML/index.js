6df5fc72a030cdcc2c8209aed28af6293a733f53
MateusMAART <101677318+MatteusMart@users.noreply.github.com>
2022-11-30 22:23:56 -0300

    Rota post inicio

--- aula04/at12-RenderizarHTML/index.js
+++ aula04/at12-RenderizarHTML/index.js
@@ -10,6 +10,16 @@ const porta = 3600
 // define a pasta publica com conteudo estatico(CSS,JS,IMG)
 app.use(express.static('views/public'))
 
+// middleware
+// rota que toda requisiçao passa por ela
+// rota padrao
+
+// decodifica os parametros enviados para a rota
+app.use(express.urlencoded({extended:true}))
+
+// coverte os valores de forma JSON
+app.use(express.json())
+
 // criar rotas
 app.get('/',(req,res)=>{
     res.status(200)
@@ -25,6 +35,21 @@ app.get('/consultar',(req,res)=>{
     res.status(200)
     res.send('<h1>Consultar</h1>')
 })
+// cria a porta para cadastrar login
+app.post('/cadastrar/login',(req,res)=>{
+    // app.send(req,body)
+    //desestruturaçao de dados
+    
+    // desestruturaçao basica campo a campo
+    // let nome = req.body.nome
+
+    // desestruturação de varios campos
+    let{nome,email,senha,confirmar} = req.body
+
+    res.send(`nome:${nome} email:${email}`)
+})
+
+
 
 // rota para retorna da pagina de erro
 // super mega importante -> ficar no final das rotas
